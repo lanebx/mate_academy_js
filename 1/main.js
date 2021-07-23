6609c534683327bdcc11b72917e4fcea51ccedfd
lanebx <havchikos@gmail.com>
2021-07-23 09:33:59 +0300

    first less

---
+++ 1/main.js
@@ -0,0 +1,17 @@
+
+
+let amount = 12;
+
+const cokie_prise = 7;
+
+const candy_prise = 8;
+
+//остача 
+let rest = amount - cokie_prise;
+
+if (rest >= candy_prise){
+    document.write('I have enouth money')
+}else{
+    document.write('I do not have enouth money')
+}
+
