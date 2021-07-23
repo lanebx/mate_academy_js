60b9235e1135a77557cdb95af65b4d0c32e8ae2b
lanebx <havchikos@gmail.com>
2021-07-23 10:03:50 +0300

    2 less

---
+++ 2/main2.js
@@ -0,0 +1,28 @@
+/* Primitive data types
+String
+Number
+BigInt
+Boolean
+undefined
+null
+Object
+Symbol 
+*/
+
+const name1 = 'lana';
+
+let age = 21;
+
+let isMarried = false;
+
+let husband = null;
+
+let x;
+
+console.log(name1, age, isMarried, husband, x);
+
+
+
+const type = typeof x;
+
+document.write(type)
