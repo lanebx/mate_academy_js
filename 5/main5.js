1bf985796c5238c9193f54e751e3986d9f0d6394
lanebx <havchikos@gmail.com>
2021-07-24 17:58:52 +0300

    5 less, String

---
+++ 5/main5.js
@@ -0,0 +1,18 @@
+const name5 = 'lana temkaeva';
+const fullName = `${name5} den`;
+const hi = `Hallo ${name5}!`
+
+console.log(fullName);
+console.log(hi);
+
+const a5 = 'aaa';
+const b5 = 'bbb';
+
+//конкатенация
+console.log(a5 +b5);
+//интерполяция
+console.log(`${a5}${b5}`);
+//екранирование
+console.log('No, it \\ is\'int.');
+
+console.log('No, it \\ is\'int.');
