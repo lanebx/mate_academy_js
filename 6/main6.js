bea10f2566be16aa13742d7afb19ee1eb0e58713
lanebx <havchikos@gmail.com>
2021-07-24 22:50:06 +0300

    6 less, function basics

---
+++ 6/main6.js
@@ -0,0 +1,12 @@
+// Rectangle 3 * 8
+
+const a = 5;
+const b = 8;
+
+const perimeter = 2 * a + b * 2;
+const square = a * b;
+
+console.log('Rectangle', a, 'x', b);
+console.log('Perimeter is ', perimeter);
+console.log('Square is ', square);
+console.log('-----------------');
