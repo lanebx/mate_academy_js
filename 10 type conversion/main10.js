0b02c3ab398629f03465bbc0342e003c207516d9
lanebx <havchikos@gmail.com>
2021-07-31 22:11:57 +0300

    10 less, type conversion

--- 10 type conversion/main10.js
+++ 10 type conversion/main10.js
@@ -25,3 +25,53 @@ console.log(
     !!conversion
 )
 
+//Conversion to number
+let value = '100.5';
+
+console.log(Number(value), 'number');
+console.log(+value, '+');
+
+console.log(parseFloat(value), 'parseFloat');
+console.log(parseInt(value, 2), 'parseInt');
+
+//Conversion to string
+
+const num = 12345;
+
+console.log(num);
+
+console.log(String(num));
+console.log(num.toString());
+console.log('' + num);
+console.log(`${num}`);
+
+
+// == vs ===
+
+console.log(
+    2 === 2.0,
+    2 == 2.0
+);
+
+console.log(
+    2 === '2.0',  //два разных типа - ЛОЖЬ
+    2 == '2.0'    //преобразовать пипы - ВЕРНО
+);
+
+console.log(
+    2 !== '2.0',  //два разных типа - true
+    2 != '2.0'    //преобразовать пипы - false
+);
+
+console.log(
+    NaN === NaN,
+    NaN == NaN,
+    Object.is(NaN, NaN)  
+);
+
+
+//Compare data of different types
+
+console.log('2' > 18);
+console.log(2 > '18');
+console.log('2' > '18');
